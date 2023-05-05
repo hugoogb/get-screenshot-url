7f249ec8b58dcb984ffcd40be84a9c8ea74813f2
Hugo GB <hugogaben8.02@gmail.com>
2023-05-06 01:12:30 +0200

    update index

--- api/index.js
+++ api/index.js
@@ -1,36 +1,43 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import puppeteer from "puppeteer-core";
 import chrome from "chrome-aws-lambda";
 
 dotenv.config();
 
-async function takeScreenshot(url) {
-	const options = {
-		args: chrome.args,
-		executablePath: await chrome.executablePath,
-		headless: chrome.headless,
-	};
-	const browser = await puppeteer.launch(options);
-	const page = await browser.newPage();
-	await page.setViewport({
-		width: 1280,
-		height: 720,
-	});
-	await page.goto(url, { waitUntil: "networkidle0" });
-	const screenshot = await page.screenshot({
-		type: "webp",
-		clip: { x: 0, y: 0, width: 1280, height: 720 },
-	});
-	await browser.close();
-	return screenshot;
-}
-
 const app = express();
 app.use(cors());
 
+// middleware
+app.use(morgan("dev"));
+
+// static "home /" page
+app.use(express.static("public"));
+
 app.get("/api/screenshot", async (req, res) => {
+	async function takeScreenshot(url) {
+		const options = {
+			args: chrome.args,
+			executablePath: await chrome.executablePath,
+			headless: chrome.headless,
+		};
+		const browser = await puppeteer.launch(options);
+		const page = await browser.newPage();
+		await page.setViewport({
+			width: 1280,
+			height: 720,
+		});
+		await page.goto(url, { waitUntil: "networkidle0" });
+		const screenshot = await page.screenshot({
+			type: "webp",
+			clip: { x: 0, y: 0, width: 1280, height: 720 },
+		});
+		await browser.close();
+		return screenshot;
+	}
+
 	const { url } = req.query;
 	if (!url) {
 		res.status(400).json({ error: "URL parameter is required" });
